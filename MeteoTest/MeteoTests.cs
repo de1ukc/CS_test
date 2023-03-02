243292ce82b5b5ea79238bff4fb3ca87988d6f6f
Aleh <Trapper.MD@mail.ru>
2023-03-03 00:30:43 +0300

    add project

---
+++ MeteoTest/MeteoTests.cs
@@ -0,0 +1,68 @@
+namespace MeteoTest
+{
+    public class MeteoTests // для тестов отключить вызов  WriteToFile в вызове Парса и закомментить зануление строки
+    {
+        [Fact]
+        public void Test1()
+        {
+            Program.MessageHandling("$15");
+            Program.MessageHandling(".16");
+            Program.MessageHandling(",");
+            Program.MessageHandling("158.3");
+
+            Assert.Equal("$15.16,158.3", Program.RecMsg);
+            Program.RecMsg = "";
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Program.MessageHandling("$11.");
+            Program.MessageHandling("12");
+            Program.MessageHandling(",18.77");
+
+            Assert.Equal("$11.12,18.77", Program.RecMsg);
+            Program.RecMsg = "";
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Program.MessageHandling("$");
+            Program.MessageHandling("1");
+            Program.MessageHandling("2");
+            Program.MessageHandling("3");
+            Program.MessageHandling(".");
+            Program.MessageHandling("4");
+            Program.MessageHandling("5");
+            Program.MessageHandling(",");
+            Program.MessageHandling("6");
+            Program.MessageHandling("7");
+            Program.MessageHandling(".");
+            Program.MessageHandling("8");
+
+            Assert.Equal("$123.45,67.8", Program.RecMsg);
+            Program.RecMsg = "";
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            Program.MessageHandling("%!№;%:$11.");
+            Program.MessageHandling("33");
+            Program.MessageHandling(",44.77+_)(*?:");
+
+            Assert.Equal("$11.33,44.77", Program.RecMsg);
+            Program.RecMsg = "";
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            Program.MessageHandling("");
+
+            Assert.Equal("", Program.RecMsg);
+            Program.RecMsg = "";
+        }
+    }
+}
--- test/Program.cs
+++ test/Program.cs
@@ -1,9 +1,11 @@
-using System.IO.Ports;
+﻿using System.IO.Ports;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 
-public class Program{
-    
+public class Program
+{
+
+    public static string RecMsg = "";
     public static int Main()
     {
         SerialPort mySerialPort = new SerialPort("COM4");
@@ -11,7 +13,7 @@ public class Program{
         mySerialPort.BaudRate = 2400;
         mySerialPort.StopBits = StopBits.One;
         mySerialPort.DataBits = 8;
-  
+
         mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
 
         mySerialPort.Open();
@@ -19,36 +21,58 @@ public class Program{
         Console.WriteLine("Приложение сериализует в 'фоне'...");
         Console.WriteLine();
         Console.ReadKey();
-        
+
         mySerialPort.Close();
 
         return 0;
     }
 
+    public static void MessageHandling(string msg)
+    {
+        /*if (String.IsNullOrEmpty(msg))
+            return;
+*/
+        if (msg.Contains('$') && !RecMsg.Contains('$'))
+        {
+            int startIndex = msg.IndexOf("$");
+            RecMsg = msg[startIndex..];
+            return;
+        }
+
+        RecMsg += msg;
+
+        string pattern = @"^\$(\d+)\.(\d\d?),(\d+)\.(\d\d?)";
+        Regex rx = new Regex(pattern, RegexOptions.Compiled);
+        MatchCollection matchCollection = rx.Matches(RecMsg);
+
+        if (matchCollection.Count > 0)
+        {
+            RecMsg = matchCollection.First().Value;
+
+            ParseFinalStringAndSerialize();
+        }
+    }
+
+    public static void ParseFinalStringAndSerialize()
+    {
+        List<string> data = RecMsg.Replace("$", "").Split(',').ToList();
+
+        double windSpeed = double.Parse(data[0], System.Globalization.CultureInfo.InvariantCulture);
+
+        double winDirection = double.Parse(data[1], System.Globalization.CultureInfo.InvariantCulture);
+
+        WriteToFile(windSpeed, winDirection);
+
+        RecMsg = "";
+    }
+
+
     private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
     {
-        try
-        {
-            SerialPort sp = (SerialPort)sender;
-            string indata = sp.ReadExisting();
+        SerialPort sp = (SerialPort)sender;
+        string indata = sp.ReadExisting();
 
-            var flag = CheckData(indata);
-
-            if (flag)
-            {
-                List<string> data = indata.Replace("$", "").Split(',').ToList();
-
-                double WindSpeed = double.Parse(data[0], System.Globalization.CultureInfo.InvariantCulture);
-
-                double WinDirection = double.Parse(data[1], System.Globalization.CultureInfo.InvariantCulture);
-
-                WriteToFile(WindSpeed, WinDirection);
-            }
-        }
-        catch
-        {
-
-        }
+        MessageHandling(indata);
     }
 
     public static void WriteToFile(double WindSpeed, double WinDirection)
@@ -66,14 +90,15 @@ public class Program{
     public static bool CheckData(string indata)
     {
         string pattern = @"^\$(\d+)\.(\d+),(\d+)\.(\d+)";
-        if (Regex.IsMatch(indata, pattern)){
+        if (Regex.IsMatch(indata, pattern))
+        {
             return true;
         }
 
         return false;
     }
 
-   // [Serializable]
+    [Serializable]
     public class Data
     {
         public string Name { get; set; } = "WMT700";
@@ -87,7 +112,7 @@ public class Program{
             WindDirection = wDirection;
             Time = DateTime.Now.ToString("G"); // dd.mm.yyyy hh.mm.ss
         }
-        
-        public Data() { } // for deserialization
+
+        public Data() { }
     }
 }
